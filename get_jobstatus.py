69e6c89de630d4ed39a9f919c8a31d3d701a1f02
kanayabuno <kana0111@g.ucla.edu>
2021-07-22 13:52:05 -0700

    .

--- get_jobstatus.py
+++ get_jobstatus.py
@@ -3,6 +3,8 @@ import requests
 import json
 
 url = 'http://192.168.99.102:31964/api/v2/'
+user = 'sj-swe'
+password = 'sj-swe'
 
 # Set proper headers
 headers = {"Content-Type":"application/json","Accept":"application/json"}
@@ -19,7 +21,7 @@ for i in range(len(data["results"])):
     user = data["results"][i]
     users[user["id"]] = user
 
-print(users.keys())
+#print(users.keys())
 
 ### List organizations
 response = requests.get(url + "organizations/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
@@ -27,23 +29,40 @@ if response.status_code != 200:
     print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
     #exit()
 
+print(response.request.url)
+print(response.request.body)
+print(response.request.headers)
 data = response.json()
 organizations = {}
 for i in range(len(data["results"])):
     organization = data["results"][i]
     organizations[organization["id"]] = organizations
 
-print(organizations.keys())
+#print(organizations.keys())
+
+### List inventories
+response = requests.get(url + "inventories/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
+    #exit()
+
+data = response.json()
+inventories = {}
+for i in range(len(data["results"])):
+    inventory = data["results"][i]
+    inventories[inventory["id"]] = inventory
+
+#print(inventories.keys())
+
 
 #### View statistics for job runs for graphing
 response = requests.get(url + "dashboard/graphs/jobs/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
 if response.status_code != 200:
-    #print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
-    print('Status:', response.status_code)
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
     #exit()
 
 data = response.json()
-print(data)
+print(json.dumps(data))
 print()
 
 # List activity streams for an organization
@@ -58,7 +77,7 @@ for i in organizations.keys():
         #exit()
 
     data = response.json()
-    print(data)
+    print(json.dumps(data, indent=1))
 
 print()
 
@@ -75,7 +94,7 @@ for i in users.keys():
         #exit()
 
     data = response.json()
-    print(data)
+    print(json.dumps(data, indent=1))
 
 ### List jobs
 response = requests.get(url + "jobs/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
@@ -84,55 +103,70 @@ if response.status_code != 200:
     #exit()
 
 data = response.json()
-print(data)
+print(json.dumps(data, indent=1))
+
 jobs = {}
 for i in range(len(data["results"])):
     job = data["results"][i]
     jobs[job["id"]] = job
 
-print(jobs.keys())
+#print(jobs.keys())
 
-### List activity streams for a job
+### Get job status
 for i in jobs.keys():
     params = {
         "id": i
     }
 
     print('jobID:', i, ' name of this job:', jobs[i]["name"], '\n')
+
+    ### List activity streams for a job
     response = requests.get(url + "jobs/" + str(i) + "/activity_stream/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
     if response.status_code != 200:
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
         #exit()
 
     data = response.json()
-    print(data)
+    print(json.dumps(data, indent=1), '\n')
 
-### List job events for a job
-for i in jobs.keys():
-    params = {
-        "id": i
-    }
-
-    print('jobID:', i, ' name of this job:', jobs[i]["name"], '\n')
+    ### List job events for a job
     response = requests.get(url + "jobs/" + str(i) + "/job_events/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
     if response.status_code != 200:
         print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
         #exit()
 
     data = response.json()
-    print(data)
+    print(json.dumps(data, indent=1), '\n')
 
+    ### List job host summaries for a job
+    response = requests.get(url + "jobs/" + str(i) + "/job_host_summaries/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
+        #exit()
 
+    data = response.json()
+    print(json.dumps(data, indent=1), '\n')
 
+    ### List Labels for a job
+    response = requests.get(url + "jobs/" + str(i) + "/labels/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
+        #exit()
 
+    data = response.json()
+    print(json.dumps(data, indent=1), '\n')
 
+### List activity streams for an inventory
+for i in inventories.keys():
+    params = {
+        "id": i
+    }
 
+    print('inventoryID:', i, ' Name:', inventories[i]["name"], '\n')
+    response = requests.get(url + "inventories/" + str(i) + "/activity_stream/", auth=('sj-swe', 'sj-swe'), headers=headers, verify=False)
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response)
+        #exit()
 
-
-
-
-
-
-
-
-
+    data = response.json()
+    print(json.dumps(data, indent=1))
